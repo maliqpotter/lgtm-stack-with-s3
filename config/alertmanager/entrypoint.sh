b52dd40d21bb3c5a02bebc59e7982976b8a10a7c
maliqpotter09 <maliqpotter09@gmail.com>
2026-08-21 21:08:14 +0700

    Fix entrypoint: use sed instead of envsubst (not available in alertmanager image)

--- config/alertmanager/entrypoint.sh
+++ config/alertmanager/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 set -e
 
-# Substitute environment variables in alertmanager config template
-# Only substitute specific variables to avoid breaking Go template syntax ({{ .Alerts }}, etc.)
-envsubst '${TELEGRAM_BOT_TOKEN} ${TELEGRAM_CHAT_ID} ${TELEGRAM_MESSAGE_THREAD_ID} ${DISCORD_WEBHOOK_URL} ${SLACK_WEBHOOK_URL}' \
-  < /etc/alertmanager/alertmanager.yml.tmpl \
-  > /etc/alertmanager/alertmanager.yml
+# Copy template to final config
+cp /etc/alertmanager/alertmanager.yml.tmpl /etc/alertmanager/alertmanager.yml
+
+# Substitute environment variables using sed (envsubst not available in this image)
+sed -i "s|\${TELEGRAM_BOT_TOKEN}|${TELEGRAM_BOT_TOKEN}|g" /etc/alertmanager/alertmanager.yml
+sed -i "s|\${TELEGRAM_CHAT_ID}|${TELEGRAM_CHAT_ID}|g" /etc/alertmanager/alertmanager.yml
+sed -i "s|\${TELEGRAM_MESSAGE_THREAD_ID}|${TELEGRAM_MESSAGE_THREAD_ID}|g" /etc/alertmanager/alertmanager.yml
+sed -i "s|\${DISCORD_WEBHOOK_URL}|${DISCORD_WEBHOOK_URL}|g" /etc/alertmanager/alertmanager.yml
+sed -i "s|\${SLACK_WEBHOOK_URL}|${SLACK_WEBHOOK_URL}|g" /etc/alertmanager/alertmanager.yml
 
 exec /bin/alertmanager "$@"
