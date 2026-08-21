1f1aef81170ba265413a291242d773bf476994b9
maliqpotter09 <maliqpotter09@gmail.com>
2026-08-21 21:17:24 +0700

    Fix entrypoint.sh: use sed instead of envsubst because prom/alertmanager image does not have envsubst

--- config/alertmanager/entrypoint.sh
+++ config/alertmanager/entrypoint.sh
@@ -2,9 +2,12 @@
 set -e
 
 # Substitute environment variables in alertmanager config template
-# Only substitute specific variables to avoid breaking Go template syntax ({{ .Alerts }}, etc.)
-envsubst '${TELEGRAM_BOT_TOKEN} ${TELEGRAM_CHAT_ID} ${TELEGRAM_MESSAGE_THREAD_ID} ${DISCORD_WEBHOOK_URL} ${SLACK_WEBHOOK_URL}' \
-  < /etc/alertmanager/alertmanager.yml.tmpl \
-  > /etc/alertmanager/alertmanager.yml
+# Using busybox sed because envsubst is not available in prom/alertmanager image
+sed -e "s|\${TELEGRAM_BOT_TOKEN}|${TELEGRAM_BOT_TOKEN}|g" \
+    -e "s|\${TELEGRAM_CHAT_ID}|${TELEGRAM_CHAT_ID}|g" \
+    -e "s|\${TELEGRAM_MESSAGE_THREAD_ID}|${TELEGRAM_MESSAGE_THREAD_ID}|g" \
+    -e "s|\${DISCORD_WEBHOOK_URL}|${DISCORD_WEBHOOK_URL}|g" \
+    -e "s|\${SLACK_WEBHOOK_URL}|${SLACK_WEBHOOK_URL}|g" \
+    /etc/alertmanager/alertmanager.yml.tmpl > /etc/alertmanager/alertmanager.yml
 
 exec /bin/alertmanager "$@"
